725ae4b81efee3a1ae81af2a0fc736de562b407c
Richard <croftrw@shaw.ca>
2016-09-27 00:58:37 -0700

    Programs to install to new OS

---
+++ programs.sh
@@ -0,0 +1,11 @@
+apt-get update
+apt-get upgrade
+apt-get install wine playonlinux darktable gimp okular
+apt-get install putty nmap aircrack-ng reaver macchanger
+apt-get install ibus ibus-anthy
+apt-get install audacity audacious rhythmbox
+apt-get install libreoffice thunderbird
+apt-get install wireshark transmission
+apt-get install kdenlive smplayer
+echo " "
+echo please manually install steam
